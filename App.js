82381f78edce983a0c4c1ac5656f017bc4fea3b0
Gamal Shaban <jeemee442@gmail.com>
2018-08-02 14:44:52 +0200

    firebase init

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import RootNavigator from './screens/RootNavigator'
+import ApiKeys from './constants/ApiKeys'
+import * as firebase from 'firebase'
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoadingCompelete: false
+    }
+    if (!firebase.apps.length) {
+      firebase.initializeApp(ApiKeys.FirebaseConfig)
+    }
+  }
   render () {
     return <RootNavigator />
   }
---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+  FirebaseConfig: {
+    apiKey: 'AIzaSyA9eCGIhC-xbrv8qbKPTsDFxy1Fz1PbzJ8',
+    authDomain: 'motawef-17fda.firebaseapp.com',
+    databaseURL: 'https://motawef-17fda.firebaseio.com',
+    projectId: 'motawef-17fda',
+    storageBucket: 'motawef-17fda.appspot.com',
+    messagingSenderId: '495095440077'
+  }
+}
