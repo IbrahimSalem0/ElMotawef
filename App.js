a4368c80aedcad95c591c6b8b752f5be0f5613c0
Gamal Shaban <jeemee442@gmail.com>
2018-08-02 21:12:26 +0200

    theme

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import RootNavigator from './screens/RootNavigator'
-import { Provider as PaperProvider } from 'react-native-paper'
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#f1c40f',
+    accent: '#3498db'
+  }
+}
 export default class App extends React.Component {
   render () {
     return (
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <RootNavigator />
       </PaperProvider>
     )
